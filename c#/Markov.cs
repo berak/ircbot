9807bb1d88de965e6f8940f2fc83d272a9b172f5
berak <px1704@web.de>
2014-07-17 20:44:39 +0200

    long overdue

---
+++ c#/Markov.cs
@@ -0,0 +1,444 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace markov3
+{
+    public class Analyzer
+    {
+        //private struct Dawg
+        //{
+        //    public int id;
+        //    public int count;
+        //    public List<string> sen;
+
+        //    public bool equals(Dawg other)
+        //    {
+        //        return false;
+        //    }
+        //};
+
+        private struct Sentence
+        {
+            public int id;
+            public string text;
+            public string dawg;
+            public List<int> words;
+            public Sentence(string t, int i)
+            {
+                id = i;
+                text = t;
+                dawg = "";
+                words = new List<int>();
+            }
+            public List<int> intersect(Sentence sen)
+            {
+                List<int> inter = new List<int>();
+                foreach (int i in words)
+                {
+                    if (sen.words.Contains(i))
+                    {
+                        if (!inter.Contains(i))
+                            inter.Add(i);
+                    }
+                }
+                return inter;
+            }
+        };
+        private struct Word
+        {
+            public int id;
+            public int count;
+            public List<int> left;
+            public List<int> right;
+            public List<int> sent;
+            public string text;
+            public string type;
+            public Word(int i, string t)
+            {
+                id = i;
+                count = 0;
+                left = new List<int>();
+                right = new List<int>();
+                sent = new List<int>();
+                text = t;
+                type = "";
+            }
+            public override string ToString()
+            {
+                string r = text + "[" + id + "," + count + "," + left.Count + "," + right.Count + "," + sent.Count + "]" + type;
+                return r;
+            }
+        };
+
+
+        List<Word> words;
+        Hashtable swords;
+        List<Sentence> dbs;
+//        List<Dawg> dawg;
+        string[] white = new string[] { " ", "\t" };
+        //string[] white = new string[] { " ", "\t", "!", ".", ",", ";", "?" };
+        public Random rand = new Random();
+        public int verbosity = 6;
+        HashSet<string> dawgs;
+
+        public Analyzer(string infile, string wdb)
+        {
+            swords = new Hashtable();
+            words = new List<Word>();
+            dbs = new List<Sentence>();
+            dawgs = new HashSet<string>();
+            readWordsDB(wdb);
+            try
+            {
+                foreach (string line in File.ReadAllLines(infile))
+                {
+                    Sentence s = addSentence(line.ToLower());
+                    dawgs.Add(s.dawg);
+                }
+            } 
+            catch (Exception) { }
+            Console.WriteLine(" dbs : " + dbs.Count);
+            Console.WriteLine(" wrd : " + words.Count);
+        }
+
+        private void readWordsDB(string infile)
+        { 
+            try
+            {
+                foreach (string line in File.ReadAllLines(infile))
+                {
+                    string[] pts = line.Split(white, StringSplitOptions.RemoveEmptyEntries);
+
+                    //FIXME whtw to do with multiple words, here ?
+                    if (pts.Length > 2) 
+                        continue;
+                    
+                    Word w = lookup( pts[0].ToLower() );
+
+                    string t = pts[pts.Length - 1];                   
+                    if ( t.StartsWith("|") )
+                        t = t.Substring(1);
+                    w.type = t;
+
+                    words[w.id] = w;
+                }
+            }
+            catch (Exception) { }
+        }
+        Word lookup(string s)
+        {
+            Word w;
+            if (!swords.ContainsKey(s))
+            {
+                int id = words.Count;
+                w = new Word(id, s);
+                words.Add(w);
+                swords[s] = id;
+            }
+            else
+            {
+                int id = (int)swords[s];
+                w = words[id];
+            }
+            return w;
+        }
+
+        Sentence addSentence(string line)
+        {
+            //foreach (Sentence s in dbs)
+            //{
+            //    if (s.text == line )
+            //        return s;
+            //}
+
+            int id = dbs.Count;
+            Sentence sen = new Sentence(line, id);
+            Word w2 = new Word(-1, "");
+            int sl = -1;
+            string[] s0 = line.Split(white, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in s0)
+            {
+                Word w = lookup(s);
+                if (sl != -1 && w2.id != -1)
+                {
+                    if (!w.left.Contains(sl)) w.left.Add(sl);
+                    if (!w2.right.Contains(sl)) w2.right.Add(w.id);
+                }
+                string t = w.type;
+                sen.dawg += t != "" ? t : "?";
+                sen.dawg += " ";
+                sl = w.id;
+                w2 = w;
+                w.sent.Add(sen.id);
+                sen.words.Add(w.id);
+                w.count = w.count + 1;
+                words[w.id] = w;
+            }
+            dbs.Add(sen);
+            return sen;
+        }
+
+        //bool checkGrammar( string ds, string wt, int pos )
+        //{
+        //    if (wt == "")
+        //        return true;
+        //    foreach (string d in dawgs)
+        //    {
+        //        if (d == "")
+        //            continue;
+        //        if (!d.StartsWith(ds))
+        //            continue;
+        //        string[] dsp = d.Split(white, StringSplitOptions.RemoveEmptyEntries);
+        //        if (pos >= dsp.Length)
+        //            return false;
+        //        if (dsp[pos].Length > wt.Length)
+        //            if (dsp[pos].Contains(wt))
+        //                return true;
+        //        if (dsp[pos].Length <= wt.Length)
+        //            if (wt.Contains(dsp[pos]))
+        //                return true;
+        //    }
+        //    return false;
+        //}
+        public string markov(string arg, int ct)
+        {
+            string[] args = arg.Split(white, StringSplitOptions.RemoveEmptyEntries);
+            if (args.Length < 1)
+                return "";
+
+            // cache words from input sentence
+            List<int> iargs = new List<int>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (swords.Contains(args[i]))
+                    iargs.Add((int)swords[args[i]]);
+            }
+            // find word with least entropy:
+            int starter = -1;
+            int startMin = 10000;
+            for (int i = 0; i < iargs.Count; i++)
+            {
+                int j = iargs[i];
+                if (words[j].count < startMin)
+                {
+                    starter = j;
+                    startMin = words[j].count;
+                }
+               //  Console.WriteLine(words[j].count + "\t" + words[j].text);
+            }
+
+            Word w = starter!=-1 ? words[ starter ] : lookup(args[0]);
+            int prv = w.id;
+            int pos = 0;
+            string txt = w.text;
+            string ds = w.type;
+            // try sometimes to expand to the left:
+            //int p = rand.Next(3);
+            //if (p > 0 && w.left.Count > 0)
+            //{
+            //    int i = rand.Next(w.left.Count - 1);
+            //    int j = w.left[i];
+            //    txt = words[j].text + " " + args[0];
+            //}
+            Word pw = w;
+            while (ct-- > 0)
+            {
+                if (w.right.Count == 0)
+                    break;
+
+                List<int> found = new List<int>();
+                foreach (int i in w.right)
+                {
+                    Word w2 = words[i];
+                    //if (!checkGrammar(ds, w2.type, pos))
+                    //    continue;
+                    // prefer words from input sentence to random
+                    if (iargs.Contains(i))
+                    {
+                        found.RemoveRange(0, found.Count); // use once only
+                        found.Add(i);
+                        iargs.Remove(i);
+                        break; // take that
+                    }
+                    if (w2.left.Contains(prv))
+                    {
+                        found.Add(i);
+                    }
+                }
+                if (found.Count == 0)
+                    break;
+
+                int j = found[rand.Next(found.Count - 1)];
+                w = words[j];
+                txt += " " + w.text;
+                if (w.text.EndsWith(".") || w.text.EndsWith("!") || w.text.EndsWith("?"))
+                    break;
+                pos++;
+                ds += " " + w.type;
+                prv = w.id;
+            }
+            return (args[0] == txt ? "" : txt);
+        }
+
+        public string match(string inp, int id)
+        {
+            Sentence sen_in = addSentence(inp);
+            List<int> found = new List<int>();
+            int ml = sen_in.words.Count / 2;
+            foreach (Sentence sen in dbs)
+            {
+                if (sen_in.text == sen.text)
+                    continue;
+                List<int> inter = sen.intersect(sen_in);
+                if (inter.Count > ml) // better weight them here!
+                {
+                    found.Add(sen.id);
+                    //ml = inter.Count;
+                }
+            }
+            if (found.Count > 0)
+            {
+                int i = rand.Next(found.Count - 1);
+                return dbs[found[i]].text;
+            }
+            return "";
+        }
+
+        public string printWord(string s, bool deep)
+        {
+            return printWord(lookup(s), deep);
+        }
+        public string printWord(int i, bool deep)
+        {
+            return printWord(words[i], deep);
+        }
+        private string printWord(Word w, bool deep)
+        {
+            string res = "";
+            try
+            {
+                res += w.ToString();
+                if (!deep)
+                {
+                    return res;
+                }
+
+                res += ("{");
+                foreach (int m in w.left)
+                    res += (m + ",");
+                res += ("}{");
+                foreach (int m in w.right)
+                    res += (m + ",");
+                res += ("}{");
+                foreach (int m in w.sent)
+                    res += (m + ",");
+                res += ("}");
+            }
+            catch (Exception)
+            {
+            }
+            return res;
+        }
+
+        public string printSentence(int id)
+        {
+            Sentence s = dbs[id];
+            //string res = s.text + "{";
+            //foreach (int m in s.words)
+            //    res += (m + ",");
+            //res += ("}");
+            //return res;
+            return s.text + "[" + s.dawg + "]";
+        }
+
+        public string run(string query)
+        {
+            if (query == null || query == ".")
+                return "";
+
+            string[] sl = query.Split(white, StringSplitOptions.RemoveEmptyEntries);
+            if (sl.Length > 0)
+            {
+                if (sl[0] == "!say")
+                {
+                    return query.Substring(5);
+                }
+                if (sl.Length == 2)
+                {
+                    if (sl[0] == "!s")
+                    {
+                        try
+                        {
+                            int idx = Int32.Parse(sl[1]);
+                            if (idx > 0)
+                                return printSentence(idx);
+                        }
+                        catch (Exception) // rand sentence containing word
+                        {
+                            if (swords.Contains(sl[1]))
+                            {
+                                int i = (int)swords[sl[1]];
+                                Word w = words[i];
+                                if (w.sent.Count > 0)
+                                {
+                                    int j = rand.Next(w.sent.Count - 1);
+                                    return printSentence(w.sent[j]);
+                                }
+                            }
+                        }
+                    }
+                    if (sl[0] == "!w")
+                    {
+                        int idx = 0;
+                        try { idx = Int32.Parse(sl[1]); }
+                        catch (Exception) { }
+                        if (idx > 0)
+                            return printWord(idx, false);
+                        else
+                            return printWord(sl[1], false);
+                    }
+
+                    if (sl[0] == "!W")
+                    {
+                        int idx = 0;
+                        try { idx = Int32.Parse(sl[1]); }
+                        catch (Exception) { }
+                        if (idx > 0)
+                            return printWord(idx, true);
+                        else
+                            return printWord(sl[1], true);
+                    }
+                    if (sl[0] == "!v")
+                    {
+                        try { verbosity = Int32.Parse(sl[1]); }
+                        catch (Exception) { }
+                        return "";
+                    }
+                }
+                if (sl.Length == 1)
+                {
+                    if (sl[0] == "!i")
+                    {
+                        return "words: " + words.Count + " " + "sentences: " + dbs.Count + " dawgs: " + dawgs.Count + " verbose: " + verbosity;
+                    }
+                    if (sl[0] == "!q")
+                    {
+                        System.Environment.Exit(1);
+                        return "bye!";
+                    }
+                }
+            }
+            string res = markov(query.ToLower(), 32); // prefered
+            //if (res == "")
+            //    res = match(query, 1); // ugly fallback
+
+            // learn a new one:            
+            addSentence(query.ToLower());
+
+            return res;
+        }
+    };
+}
---
+++ c#/Markov3.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace irc
+{
+    public interface Server
+    {
+        bool start(Client client, string ircnode, string channel);
+    }
+
+    public interface Client
+    {
+        string nick();
+        string run(string line);
+    }
+
+    public class Bot : Server
+    {
+        private Client client = null;
+        private TcpClient irc = null;
+        private StreamWriter writer;
+        private StreamReader reader;
+
+        private static int PORT = 6667;
+        private static string SERVER = "irc.freenode.net";
+        private static string NICK = "marc0ni"; 
+        private static string PASS = "i_am_" + NICK;
+        public static string CHANNEL = "#Markoff";
+        private string[] moreChannels = new string[] { };
+                
+        public bool start(Client client, string ircnode, string channel)
+        {
+            try
+            {
+                this.client = client;
+                CHANNEL = channel;
+                SERVER = ircnode;
+                NICK = client.nick();
+
+                this.irc = new TcpClient(SERVER, PORT);
+                Stream stream = irc.GetStream();
+                this.reader = new StreamReader(stream);
+                this.writer = new StreamWriter(stream);
+
+                write("PASS " + "(none)");
+                write("USER " + NICK + " 12 * :" + NICK  );
+                write("NICK " + NICK);
+                write("JOIN " + channel);
+
+                listen();
+            }
+            catch (Exception ex) { Console.WriteLine(ex); Console.WriteLine(SERVER + " " + CHANNEL); return false; }
+            return true;
+        }
+
+        string read()
+        {
+            string mess = "";
+            try
+            {
+                mess = reader.ReadLine();
+                Console.WriteLine(mess);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return mess;
+        }
+        public void write(string mess)
+        {
+            try
+            {
+                this.writer.WriteLine(mess);
+                this.writer.Flush();
+                Console.WriteLine(mess);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(mess + "\n" + e.ToString());
+            }
+        }
+        public void writeMess(string to, string mess)
+        {
+            if (mess == null || mess.Length < 1)
+                return;
+            if (mess.Length > 460)
+            {
+                mess = mess.Substring(0, 460) + "[truncated]";
+            }
+            write("PRIVMSG " + to + " :" + mess + "\r\n");
+        }
+
+        string messFrom(string m, char sep)
+        {
+            int endName = m.IndexOf(sep) - 1;
+            if (endName > 0 && endName < m.Length)
+                return m.Substring(1, endName);
+            return null;
+        }
+
+
+        void listen()
+        {
+            bool ready = false;
+            string inputLine = null;
+            while ((inputLine = read()) != null)
+            {
+                int pg = inputLine.IndexOf("PING :");
+                if (pg >= 0)
+                {
+                    pg += 6;
+                    write("PONG :" + inputLine.Substring(pg, inputLine.Length - pg));
+                    continue;
+                }
+                // skip motd, etc.
+                if (inputLine.IndexOf("End of /MOTD command.")>= 1)
+                {
+                    ready = true;
+                    foreach (string c in moreChannels)
+                    {
+                        write("JOIN " + c);
+                    }
+                    continue;
+                }
+                if (ready)
+                {
+
+                    string res = client.run(inputLine);
+                    if (res != null && res != "")
+                    {
+                        if (res.StartsWith("/"))
+                            write(res.Substring(1));
+                        else
+                            writeMess(CHANNEL, res);
+                    }
+                }
+            } 
+        }
+    };
+
+    public class Konsole : irc.Client
+    {
+        private string me = "", cin = "";
+        Thread listen = null;
+        Bot server = null;
+        public Konsole(Bot serv, string n)
+        {
+            this.me = n;
+            this.server = serv;
+            listen = new Thread(new ThreadStart(delegate()
+            {
+                while (true)
+                {
+                    cin = Console.ReadLine();
+                    if ( cin.StartsWith("/") )
+                        server.write( cin.Substring(1) );
+                    else
+                        server.writeMess(Bot.CHANNEL, cin);
+                }
+            }));
+            listen.Start();
+        }
+        public string nick() { return me; }
+        public string run(string line)
+        {
+            return "";
+        }
+    }
+
+ 
+}
+
+namespace markov3
+{
+    public class MarkovBot : irc.Client
+    {
+        markov3.Analyzer analyze;
+        private string me = "";
+        irc.Bot server = null;
+        public MarkovBot(irc.Bot serv, string n, string txt)
+        {
+            this.analyze = new markov3.Analyzer(txt, "E:\\code\\cs\\net\\gIOrdi\\markov3\\part-of-speech.txt");
+            this.me = n;
+            this.server = serv;
+        }
+        public string nick() { return me; }
+
+        public string run(string line)
+        {
+            string cout = "";
+            string[] messit = line.Split(new string[] { "PRIVMSG " }, StringSplitOptions.RemoveEmptyEntries);
+            if (messit.Length < 2)
+                return "";
+
+            int n = messit[1].IndexOf(":");
+            if (n >= 0)
+            {
+                string s = messit[1].Substring(n + 1);
+                bool takeit = (1 == analyze.rand.Next(analyze.verbosity));
+                takeit |= s.ToLower().Contains(me.ToLower());
+                takeit |= s.Contains("All:");
+                n = s.IndexOf(":");
+                if (n >= 0 && n <= 12)
+                {
+                    s = s.Substring(n + 1);
+                }
+                n = s.IndexOf(",");
+                if (n >= 0 && n <= 12)
+                {
+                    s = s.Substring(n + 1);
+                }
+                if (takeit)
+                {
+                    cout = analyze.run(s);
+                    Thread.Sleep(300); // catch up on latest
+                }
+            }
+            return cout;
+        }
+    }
+    class Markov3
+    {
+        static void Main(string[] args)
+        {
+            if (true)
+            {
+                irc.Bot bot = new irc.Bot();
+                if (args[0] == "con")
+                {
+                    bot.start(new irc.Konsole(bot, "n3rv0us"), "irc.freenode.net", "#p4p4p4");
+                }
+                else
+                {
+                    string a0 = "no.txt";
+                    if (args.Length > 0) a0 = args[0];
+                    string a1 = "empty";
+                    if (args.Length > 1) a1 = args[1];
+                    string a2 = "#p4p4p4";
+                    if (args.Length > 2) a2 = args[2];
+                    bot.start(new MarkovBot(bot, a1, a0), "irc.freenode.net", a2);
+                }
+            }
+            //else 
+            //{
+            //    Analyzer analyze = new Analyzer("E:\\code\\cs\\net\\gIOrdi\\markov3\\darwin1.txt", "E:\\code\\cs\\net\\gIOrdi\\markov3\\part-of-speech.txt");
+            //    while (true)
+            //    {
+            //        Console.WriteLine(analyze.run(Console.ReadLine()));
+            //    }
+            //}
+        }
+    }
+}
