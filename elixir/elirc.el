a0334052fe976ac15be34fdf311b606bae5bf0f9
berak <px1704@web.de>
2015-06-22 13:25:36 +0200

    elixir

--- elixir/elirc.el
+++ elixir/elirc.el
@@ -1,37 +1,41 @@
 defmodule Irc do
   def connect(chan,nick) do
     opts = [:binary, packet: :line, active: false]
-    {:ok, sock} = :gen_tcp.connect('irc.freenode.net', 6667, opts)
-    intro = "USER elirc 12 * :elirc\r\n"
-         <> "NICK " <> nick <> "\r\n" 
-         <> "PASS i_am" <> nick <> "\r\n" 
-         <> "JOIN " <> chan <> "\r\n"
-    :gen_tcp.send(sock, intro)
-    Task.start_link(fn -> rv(sock) end)
-    kb(sock)
+    case :gen_tcp.connect('irc.freenode.net', 6667, opts) do
+      {:ok, sock} -> :gen_tcp.connect('irc.freenode.net', 6667, opts)
+        intro = "USER elirc 12 * :elirc\r\n"
+             <> "NICK " <> nick <> "\r\n"
+             <> "PASS i_am" <> nick <> "\r\n"
+             <> "JOIN " <> chan <> "\r\n"
+        :gen_tcp.send(sock, intro)
+        Task.start_link(fn -> rcv(sock) end)
+        kb(sock, chan, nick)
+      {:error, msg} -> IO.puts msg
+    end
   end
 
-  defp rv(sock) do
+  defp rcv(sock) do
     case :gen_tcp.recv(sock, 0, 99999999) do
-      {:ok, "PONG"} ->
-        :gen_tcp.send(sock, "PONG, y'old bastard\r\n")
-        rv(sock)
       {:ok, m} ->
         IO.puts m
-        rv(sock)
-      {:error, :timeout} -> 
-        :ok
+        if String.starts_with?(m, "PING") do
+          :gen_tcp.send(sock, "PONG lalala\r\n")
+        end
+        rcv(sock)
+      {:error, reason} ->
+        System.halt(1)
     end
   end
-  
-  defp kb(sock) do
-    m = IO.gets "> "
-    cond do
-      m == "ok.\n" -> 1
-      true -> 
-        :ok = :gen_tcp.send(sock, m)
-        kb(sock)
+
+  defp kb(sock, chan, nick) do
+    m = IO.gets "$ "
+    if String.starts_with?(m, "/") do
+      s = String.slice(m, 1,10000)
+    else
+      s = "PRIVMSG " <> chan <> " :" <> m
     end
+    :gen_tcp.send(sock, s)
+    kb(sock, chan, nick)
   end
 end
 
